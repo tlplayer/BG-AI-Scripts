c3a54a24f14d4193865a4828e46cf3794e31c10d
tlplayer <timothy.player@comcast.net>
2026-07-15 12:32:54 -0400

    Back and forth states now work well for black pits to baldur's gate and vice versa

---
+++ black_pits_eet_arena/dialogue/bpea_bpghlour.d
@@ -0,0 +1,7 @@
+EXTEND_BOTTOM ~BPGHLOUR~ 13
+  IF ~~ THEN REPLY ~Can you send us back to the Friendly Arm Inn?~ DO ~ActionOverride(Player1,LeaveAreaLUA("BG2301","",[500.500],6)) ActionOverride(Player2,LeaveAreaLUA("BG2301","",[462.528],6)) ActionOverride(Player3,LeaveAreaLUA("BG2301","",[538.528],6)) ActionOverride(Player4,LeaveAreaLUA("BG2301","",[462.472],6)) ActionOverride(Player5,LeaveAreaLUA("BG2301","",[538.472],6)) ActionOverride(Player6,LeaveAreaLUA("BG2301","",[500.548],6))~ EXIT
+END
+
+EXTEND_BOTTOM ~BPGHLOUR~ 14
+  IF ~~ THEN REPLY ~Can you send us back to the Friendly Arm Inn?~ DO ~ActionOverride(Player1,LeaveAreaLUA("BG2301","",[500.500],6)) ActionOverride(Player2,LeaveAreaLUA("BG2301","",[462.528],6)) ActionOverride(Player3,LeaveAreaLUA("BG2301","",[538.528],6)) ActionOverride(Player4,LeaveAreaLUA("BG2301","",[462.472],6)) ActionOverride(Player5,LeaveAreaLUA("BG2301","",[538.472],6)) ActionOverride(Player6,LeaveAreaLUA("BG2301","",[500.548],6))~ EXIT
+END
